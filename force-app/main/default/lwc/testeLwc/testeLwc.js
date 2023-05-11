8b965f022e2468676484d6560ae285e60cf0fc19
Eduardo Ferreira <edu.dferreira@hotmail.com>
2023-05-11 10:50:27 -0300

    criado LWC, aura e visualforce page

---
+++ force-app/main/default/lwc/testeLwc/testeLwc.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+
+export default class TesteLwc extends LightningElement {
+    newTest = 'ola mundo!';
+}
--- force-app/main/default/triggers/CasaTrigger.trigger
+++
@@ -1,3 +0,0 @@
-trigger CasaTrigger on Casa__c (before insert, before update) {
-    CasaTriggerHandler.alteraCampo(Trigger.new);
-}
